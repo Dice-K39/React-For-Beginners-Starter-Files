4b7c727c03b1904dccc92c97d971955c44ce1a93
Dice-K39 <daisukehashimoto39@gmail.com>
2021-01-02 00:13:43 -0500

    Finished video 16.

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends Component
 {
@@ -34,12 +35,30 @@ class App extends Component
         );
     }
 
+    addToOrder = (key) =>
+    {
+        const order = {...this.state.order};
+
+        order[key] = order[key] + 1 || 1;
+        this.setState({order});
+    }
+
     render()
     {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market" />
+                    <ul className="fishes">
+                        {Object.keys(this.state.fishes).map(key => 
+                            <Fish 
+                                key={key} 
+                                index={key}
+                                details={this.state.fishes[key]}
+                                addToOrder={this.addToOrder} 
+                            />
+                        )}
+                    </ul>
                 </div>
 
                 <Order />
